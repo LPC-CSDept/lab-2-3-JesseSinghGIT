9a2f34ba3c08f68d884ce8c07e44931bc3f4e4f4
JesseSinghGIT <123704475+JesseSinghGIT@users.noreply.github.com>
2023-02-08 12:03:59 +0000

    program

--- main.py
+++ main.py
@@ -3,6 +3,21 @@ def main():
     # Comlete your code here
     ##################################################
 
+
+    val1 = int(input ('Enter your first number '))
+    val2 = int(input('Enter your second number '))
+    val3 = int(input('Enter your third number '))
+    sum = val1 + val2 + val3 
+    print ('Values: ', val1, val2, val3)
+    print ('Total:  \t ', sum)
+    avg = sum/3
+    print (f'Average: \t {avg: .2f}')
+    # print ('Average: \t {0: .2f}'.format(avg))
+
+
+
+
+
     pass
 
 
